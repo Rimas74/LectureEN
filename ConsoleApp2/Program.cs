7c923398276ba603fefb01b834630aabea396b33
Rimas74 <gintilas.rimantas@gmail.com>
2023-10-30 22:08:48 +0200

    new changes

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -3,13 +3,11 @@
     internal class Program
         {
         static void Main(string[] args)
-            {
-           
+            {           
             string preke = "Obuolys";
             int quantity = 5;
             string price = "5 EUR";
             Console.WriteLine($"Preke:{preke}\nKiekis:{quantity} \nKaina:{price}");
-
             }
         }
     }
