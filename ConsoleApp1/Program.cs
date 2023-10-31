bd5415f0cf096ab8d9c0b78368ad96e8ceaab644
Rimas74 <gintilas.rimantas@gmail.com>
2023-10-31 11:51:34 +0200

    Tasks 2 and 3 in Task1_4 included

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp1
         {
         static void Main(string[] args)
             {
+            //Task_1
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("    /\\    ");
@@ -13,6 +14,35 @@ namespace ConsoleApp1
             Console.WriteLine("  /    \\  ");
             Console.WriteLine(" /______\\ ");
             Console.WriteLine();
+            Console.WriteLine(new string('-', 20));
+
+            //Task_2
+            Console.WriteLine(new string(' ',5) + new string('*',5));
+            Console.WriteLine(new string(' ',3) + new string('*',2)+ new string(' ', 5)+ new string('*', 2));
+            Console.WriteLine(new string(' ',1) + new string('*',2)+ new string(' ', 9)+ new string('*', 2));
+            Console.WriteLine(new string(' ',0) + new string('*',2)+ new string(' ', 3)+"Hello" + new string(' ', 3) + new string('*', 2));
+            Console.WriteLine(new string(' ',0) + new string('*',2)+ new string(' ', 4)+ "from"+ new string(' ', 3)+new string('*', 2));
+            Console.WriteLine(new string(' ',1) + new string('*',2)+ new string(' ', 3)+"C#" + new string(' ', 4) + new string('*', 2));
+            Console.WriteLine(new string(' ',3) + new string('*',2)+ new string(' ', 5)+ new string('*', 2));
+            Console.WriteLine(new string(' ',5) + new string('*', 5));
+            Console.WriteLine(new string('-', 20));
+
+            //Task_3
+
+            
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("    /\\    ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("   /  \\   ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("  /    \\  ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(" /______\\ ");
+            Console.WriteLine();
+            Console.WriteLine(new string('-', 20));
+
+            //Task_4
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.WriteLine("  / \\     / \\");
